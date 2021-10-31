3f3453868c880436671924c7a8d592145f770c94
Krystian <krystian.gryczon2@gmail.com>
2021-10-31 11:57:43 +0100

    Rendering filtrable list on user page

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,8 +1,14 @@
+import { FiltrableUsersList } from 'components';
 import React from 'react';
 import './Users.scss';
 
 const Users = () => {
-	return <div></div>;
+	return (
+		<div className="usersPageContainer">
+			<h2>Users list</h2>
+			<FiltrableUsersList />
+		</div>
+	);
 };
 
 export default Users;
