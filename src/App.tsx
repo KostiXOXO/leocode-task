5213b9c8a7f4cbc37cd86232ec27a3aad931b577
Krystian <krystian.gryczon2@gmail.com>
2021-10-31 11:03:39 +0100

    initial commit
    created repo, set up linting
    set up app entry

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { MainPage } from 'pages';
+import './App.scss';
+
+function App() {
+	return (
+		<div className="appContainer">
+			{/* <MainPage /> */}
+			dasdas
+			<button data-testid="button"></button>
+		</div>
+	);
+}
+
+export default App;
