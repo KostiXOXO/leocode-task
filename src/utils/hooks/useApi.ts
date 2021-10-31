2894a37b4d7d4058aec2501d458c1eac045542b8
Krystian <krystian.gryczon2@gmail.com>
2021-10-31 12:28:48 +0100

    UseApi hook error message improvement

--- src/utils/hooks/useApi.ts
+++ src/utils/hooks/useApi.ts
@@ -2,18 +2,18 @@ import { useState, useEffect } from 'react';
 import { IUser } from 'utils/interfaces/IUser';
 
 const useApi = (apiFunction: Function) => {
-	const [data, setData] = useState<IUser[] | null>(null);
 	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState<IUser[] | null>(null);
 	const [error, setError] = useState<string | null>(null);
 
 	useEffect(() => {
 		apiFunction()
-			.then((data: IUser[]) => {
+			.then(({ data }: { data: IUser[] }) => {
 				setData(data);
 				setIsLoading(false);
 			})
 			.catch(() => {
-				setError('Something went wrong');
+				setError('Something went wrong retrieving users list');
 				setIsLoading(false);
 			});
 	}, [apiFunction]);
