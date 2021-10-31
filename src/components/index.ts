564f8f6363615ab9d34a0bf1406fd78a074f0b01
Krystian <krystian.gryczon2@gmail.com>
2021-10-31 11:56:10 +0100

    Created Spinner component in case of slow net

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import UsersList from './list/UsersList';
+import SearchBox from './searchBox/SearchBox';
+import FiltrableUsersList from './filtrableUsersList/FiltrableUsersList';
+import Spinner from './spinner/Spinner';
+
+export { UsersList, SearchBox, FiltrableUsersList, Spinner };
