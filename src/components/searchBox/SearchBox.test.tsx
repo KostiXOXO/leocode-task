61adf1bccd1039689d0f507badb490d27a7e5427
Krystian <krystian.gryczon2@gmail.com>
2021-11-01 21:29:43 +0100

    Set up jest-axe and fixed the test using it

--- src/components/searchBox/SearchBox.test.tsx
+++ src/components/searchBox/SearchBox.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { cleanup, render, screen } from '@testing-library/react';
 import { axe } from 'jest-axe';
-import userEvent from '@testing-library/user-event';
 
 import SearchBox from './SearchBox';
 
@@ -9,7 +8,7 @@ afterEach(cleanup);
 
 it('Should not fail any accessibility tests', async () => {
 	const { container } = render(<SearchBox placeholder="Search test..." value="test" onChange={jest.fn()} />);
-	expect(await axe(container)).toHaveNoViolations;
+	expect(await axe(container)).toHaveNoViolations();
 });
 
 it('Placeholder should be applied', () => {
